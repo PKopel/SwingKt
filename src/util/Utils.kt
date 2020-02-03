ebd9f6c68a1c327948a7be92d62d534ad2b91a4f
PKopel <pawel.kopel2@gmail.com>
2020-02-03 16:50:30 +0100

    improve formatting

--- src/util/Utils.kt
+++ src/util/Utils.kt
@@ -3,8 +3,6 @@ package util
 import java.awt.Color
 import java.awt.event.ActionEvent
 import javax.swing.*
-import javax.swing.event.AncestorEvent
-import javax.swing.event.AncestorListener
 
 fun run(f: JFrame, width: Int, height: Int, name: String? = null) {
     SwingUtilities.invokeLater {
@@ -15,16 +13,18 @@ fun run(f: JFrame, width: Int, height: Int, name: String? = null) {
     }
 }
 
-fun getRGBColor(r: Int, g: Int, b:Int): Color{
+fun getRGBColor(r: Int, g: Int, b: Int): Color {
     val hsb = FloatArray(3)
-    Color.RGBtoHSB(r,g,b,hsb)
-    return Color.getHSBColor(hsb[0],hsb[1],hsb[2])
+    Color.RGBtoHSB(r, g, b, hsb)
+    return Color.getHSBColor(hsb[0], hsb[1], hsb[2])
 }
 
-fun radioMenuItem(text: String = "",
-                  background: Color = Color.LIGHT_GRAY,
-                  listener: (ActionEvent) -> Unit = {},
-                  selected: Boolean = false): JRadioButtonMenuItem {
+fun radioMenuItem(
+    text: String = "",
+    background: Color = Color.LIGHT_GRAY,
+    listener: (ActionEvent) -> Unit = {},
+    selected: Boolean = false
+): JRadioButtonMenuItem {
     val item = JRadioButtonMenuItem(text)
     item.isSelected = selected
     item.background = background
@@ -32,22 +32,26 @@ fun radioMenuItem(text: String = "",
     return item
 }
 
-fun button(text: String = "",
-           background: Color = Color.LIGHT_GRAY,
-           listener: (ActionEvent) -> Unit = {}): JButton {
+fun button(
+    text: String = "",
+    background: Color = Color.LIGHT_GRAY,
+    listener: (ActionEvent) -> Unit = {}
+): JButton {
     val button = JButton(text)
     button.background = background
     button.addActionListener(listener)
     return button
 }
 
-fun textField(text: String = "",
-              background: Color = Color.LIGHT_GRAY,
-              editable: Boolean = true,
-              listener: (ActionEvent) -> Unit = {}): JTextField{
+fun textField(
+    text: String = "",
+    background: Color = Color.LIGHT_GRAY,
+    editable: Boolean = true,
+    listener: (ActionEvent) -> Unit = {}
+): JTextField {
     val field = JTextField(text)
-    field.background=background
-    field.isEditable=editable
+    field.background = background
+    field.isEditable = editable
     field.addActionListener(listener)
     return field
 }
